890127cbcd5ed0e9c4ff5551d407a20ce395d4f9
Your Name <you@example.com>
2024-07-31 16:38:37 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-xl underline text-white">Home</h1>
+      <Button className="bg-blue-300">Click me</Button>
+    </>
+  );
+}
